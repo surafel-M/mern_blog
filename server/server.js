e3936eea2fe4a6852fb8a725de07d4d2c225f8a7
Surafel Destaw <surafel.destaw01@gmail.com>
2026-04-17 08:01:54 +0300

    Database integration: MongoDB Atlas connection with Mongoose and user creation test

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import User from "./models/User.js";
+
 
 dotenv.config();
+
+// Connect database
+connectDB();
 
 const app = express();
 
@@ -14,6 +20,17 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.get("/test-user", async (req, res) => {
+  const user = await User.create({
+    username: "testuser",
+    email: "test@test.com",
+    password: "123456",
+  });
+
+  res.json(user);
+});
+
 
 // Port
 const PORT = process.env.PORT || 6000;
