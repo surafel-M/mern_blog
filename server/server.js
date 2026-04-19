3c1d94f07e3fe4e10ce9b6576ef0447f3ad8d4c0
Surafel Destaw <surafel.destaw01@gmail.com>
2026-04-19 11:05:30 +0300

    Update base API route to /api

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
 // Test route
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("API is running...");
 });
 
