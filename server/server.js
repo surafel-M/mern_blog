14f5860fcd9673d458e874c41a9d9e585778f05a
Surafel Destaw <surafel.destaw01@gmail.com>
2026-05-03 10:47:59 +0300

    Deploy backend

--- server/server.js
+++ server/server.js
@@ -17,7 +17,9 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
